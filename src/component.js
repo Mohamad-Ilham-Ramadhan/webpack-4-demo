ac65d7c882518791b0b807c8e647580b4b107c5c
Mohamad Ilham Ramadhan <mohamadilhamramadhan@gmail.com>
2018-04-09 14:57:37 +0700

    Developing -webpack-dev-server

--- src/component.js
+++ src/component.js
@@ -1,7 +1,11 @@
+const path = require("path");
 export default( text = 'Hello Webpack 4') => {
 	const element = document.createElement('div');
 
 	element.innerHTML = text;
+
+	const x = path.resolve(__dirname);
+	console.log( __dirname );
 
 	return element;
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -6,4 +7,21 @@ module.exports = {
 			title: 'Belajar Webpack 4',
 		}),
 	],
+
+	devServer: {
+		contentBase: path.join(__dirname, "meledog"),
+		// Display only errors to reduce the amount of output.
+		stats: 'errors-only',
+
+		// Parse host and port from env to allow customization.
+		// 
+		// If you use Docker, Vargant or Cloud9, set host: options.host || "0.0.0.0" 
+
+		// 0.0.0.0 is availabel to all network devices 
+		// unlike default localhost. 
+		host: process.env.HOST, // Default to 'localhost'
+		port: process.env.PORT, // Default to 8080
+		open: true, // Open the page in browser 
+		overlay: true,
+	},
 }
