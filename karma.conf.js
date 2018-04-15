43ca45a46eedae77a614ac4ad140f71128397c96
Mohamad Ilham Ramadhan <mohamadilhamramadhan@gmail.com>
2018-04-15 15:14:42 +0700

    Techniques -Testing

---
+++ karma.conf.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const parts = require("./webpack.parts");
+
+module.exports = config => {
+	const tests = "tests/*.test.js";
+
+	process.env.BABEL_ENV = "karma";
+
+	config.set({
+		frameworks: ["mocha"],
+		files: [
+			{
+				pattern: tests
+			}
+		],
+		preprocessors: {
+			[tests]: ["webpack"],
+		},
+		webpack: parts.loadJavaScript(),
+		singleRun: true,
+		browsers: ["PhantomJS"],
+
+		reporters: ["coverage"],
+		coverageReporter: {
+			dir: "build",
+			reporters: [{ type: "html" }, { type: "lcov"}]
+		}
+	});
+}
